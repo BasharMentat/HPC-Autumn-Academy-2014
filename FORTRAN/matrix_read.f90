b85de8907154b060f761821872af23c2bdbdb81c
Mentat <melccel@gmail.com>
2014-09-15 01:38:30 +0100

    folder reshuffle, reading finished!

---
+++ FORTRAN/matrix_read.f90
@@ -0,0 +1,19 @@
+!program that reads a matrix using a function
+PROGRAM read_mat
+
+INTEGER :: i,j
+
+REAL, DIMENSION(2,3) :: A
+
+DO i=1,2
+  DO j=1,3
+    READ (*,*) A(i,j)
+  ENDDO
+ENDDO
+
+DO i=1,2
+  WRITE(*,10)  (A(i,j),j=1,3)
+END DO
+10  FORMAT(10f5.1)   
+
+END PROGRAM read_mat
